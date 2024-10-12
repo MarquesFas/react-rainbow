dc41bbd101165ebef0db300ebb869b5a1c277616
MarquesFas <102706829+MarquesFas@users.noreply.github.com>
2024-10-12 12:49:35 -0400

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -11,15 +11,23 @@ function App(){
     
     let colorMap = colors.map((color, i) => {
         return (
-            <ColorBlock color={color} />
-           const addColor = (newColor) => {
-              setColors([...colors, newColor])
+            <ColorBlock key={i} color={color} />
+        )
+    })
+
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
     }
+
     return (
         <div className="App">
             {colorMap}
             <ColorForm addColor={addColor} />
+
         </div>
+
     )
 
-       
+}
+
+export default App
